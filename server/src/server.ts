570e17dd90a6075d343b8a7663ee60f4853dc075
KRyderTR <charitrom@gmail.com>
2026-02-12 13:16:40 +0200

    db connected

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,17 @@
 import "dotenv/config";
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+(async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is missing. Check server/.env");
+  }
+
+  await connectDB(process.env.MONGO_URI);
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+})();
